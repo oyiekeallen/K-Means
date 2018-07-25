a682fe77ea94740b02e084dc857dbbc86028d300
Oyieke Allen <oyiekeallen@gmail.com>
2018-07-25 15:19:43 +0300

    Going public

---
+++ README.md
@@ -0,0 +1,49 @@
+# K-MEANS CLUSTERING
+
+## Overview
+
+Implementation of a simple but powerful clustering algorithm for 2D ('x' 'y') points . Simple and optimal for most data structures that are crawled iteratively, large list searches are avoided, some calculations are strict, and so on. Additionally, the total time of program operation (including IO operations, input parsing and output formatting), and how many iterations the algorithm has completed is measured. Finally, an intra-cluster sum of squares is calculated - the sum of the squares of the distances between each point in a cluster and the cluster center (for all clusters in total). This amount actually tries to minimize the algorithm, but may not reach a global minimum depending on the selected start centers (NP-hard is the task of finding a global minimum). For convenience, the minimum reached is displayed on the standard output, along with the number of iterations and program runtime.
+
+if using method two \#2
+* The 'clock` package (https://hackage.haskell.org/package/clock) is required to accurately measure the time (without it the code, of course, does not compile).
+
+One of the most significant and effective tricks is shown in the 'reClusterize' function, which with a linearly iterative and strict crawling of the complete list of points calculates for each of them its new cluster and at the same time monitors whether one changes it , and returns this information as a `Bool` into an ordered pair with the resulting list of points. The same technique for retrieving all the required information with only merged and strict list crawling is also used in the mean and mapAndLength functions.
+
+On the other hand, the `groupToClusters` function can be greatly improved as it is also invoked once in each iteration, and it shines with simplicity rather than efficiency. There are other features that can be optimized in ways the compiler simply can not guess, and may choose different data structures (such as unboxed lists and / or vectors), but these are long-distance goals for improvement. In any case, this is a proof of concept.
+
+The code can be executed with the 'kmeans :: String -> Int -> String -> IO () `function, which gets three arguments:
+
+* filename of the input data - a text file in which each row contains the coordinates of a single point separated by an empty space
+* Number of search clusters
+* file name for output data - clustering result.
+
+## Execution
+
+__METHOD \#1__
+The easy way(stack):
+1. Clone the repository
+
+2. Build the project:
+> - stack build
+
+3. Execute the program 
+> - stack exec k-means-exe
+
+__METHOD \#2__
+Of course, for faster execution, the code can be compiled into a executable, which at startup gets three similar command-line arguments:
+```
+> - ghc app/Main.hs
+> - . \ k-mean.exe example/input.txt 4 example/output.txt --> General systax (program inputfile clusters outputfile)
+```
+Arguments are not validated, only their number is tracked (at least three, all after the third are ignored). The only validation of input data is whether the number of desired clusters does not exceed the amount of data.
+
+The file [`input.txt`](./input.txt) serves as an example input and contains 1249 points without any particular internal structure 
+
+## Runtime
+On my non-dedicated laptop clustering in 4 clusters takes an average of ~ 0.3 seconds and in 10 clusters - about 0.7 seconds.
+
+## RESULTS
+The result of execution are stored as a text file named output.txt inside the directory example.
+
+## REPORT
+View pdf in Documentation folder.
---
+++ app/Main.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Main (main, kmeans) where
+
+import System.Environment (getArgs, withArgs)
+import System.Random (mkStdGen, randomRs)
+import System.Clock (TimeSpec, Clock(Realtime), getTime, diffTimeSpec, toNanoSecs) -- requires "clock" package
+import System.IO (readFile, writeFile, hFlush, stdout)
+import Data.List (foldl', minimumBy, nub)
+import Data.Ord (comparing)
+
+class Clusterizable a where
+  dist      :: a -> a -> Double
+  mean      :: [a] -> a
+  fromFile  :: String -> a
+  toFile    :: a -> String
+
+type Point = (Double,Double)
+instance Clusterizable Point where
+  dist (a,b) (c,d)  = (a-c)^2 + (b-d)^2
+  mean pts          = let (xs,ys,len) = foldl' (\(ax,ay,len) (x,y) -> (ax+x,ay+y,len+1)) (0,0,0) pts in (xs / len, ys / len)
+  fromFile str      = let (x:y:_) = words str in (read x, read y)
+  toFile (x,y)      = show x ++ " " ++ show y
+
+-- Strict and fused map + length (+ reverse)
+mapAndLength :: (a -> b) -> [a] -> ([b],Int)
+mapAndLength f = foldl' (\(result,len) x -> ((f x):result, len+1)) ([],0)
+
+-- Main heavy-lifting function, generating the initial centers and clusters and passing them to the looping nextIteration function.
+-- Receives the randomly generated indices of the initial cluster centers.
+-- Returns the final clusters as well as the number of iterations needed.
+-- Each point is given an index in the range [1..k], showing to which cluster it belongs.
+clusterize :: Clusterizable a => Int -> [Int] -> [a] -> ([[a]], Int)
+clusterize k centerIdxs points = nextIteration k initialCenters initialClusters 1
+  where initialCenters = zip (map (points!!) centerIdxs) [1..]
+        initialClusters = fst $ reClusterize initialCenters [(p,-1) | p<-points]
+
+-- At every step this function calculates the new cluster centers,
+-- reclusterizes the points and breaks when no point has changed its cluster.
+nextIteration :: Clusterizable a => Int -> [(a,Int)] -> [(a,Int)] -> Int -> ([[a]], Int)
+nextIteration k centers points iter
+  | isStable  = (groupToClusters k newpoints, iter)
+  | otherwise = nextIteration k newcenters newpoints (iter+1)
+  where newcenters = calculatemeans k points
+        (newpoints,isStable) = reClusterize newcenters points
+
+-- Groups the indexed points in simple lists by their cluster index.
+groupToClusters :: Int -> [(a,Int)] -> [[a]]
+groupToClusters k points = [ [ p | (p,j)<-points, i==j ] | i<-[1..k]]
+
+-- Calculates all clusters' means by firstly grouping the indexed points by clusters.
+calculatemeans :: Clusterizable a => Int -> [(a,Int)] -> [(a,Int)]
+calculatemeans k points = zip (map mean $ groupToClusters k points) [1..]
+
+-- Recalculates the nearest center for every point, while keeping
+-- check whether any single point has changed its cluster. Done with
+-- a simple strict fold of the indexed point list.
+reClusterize :: Clusterizable a => [(a,Int)] -> [(a,Int)] -> ([(a,Int)], Bool)
+reClusterize centers = foldl' (\(result,flag) (p,i) -> let newI = snd $ minimumBy (comparing (dist p . fst)) centers
+                                                       in ((p,newI):result, flag && newI == i)) ([],True)
+
+-- Quantify the minimum reached with the clusterization. The algorithm basically
+-- tries to assign each point to a cluster, in a way that minimizes this amount.
+-- It is being printed on the standard output in order for the user to judge whether
+-- a local or a global minimum has been reached. WCSS stands for within-cluster sum of squares.
+wcss :: Clusterizable a => [[a]] -> Double
+wcss clusters = sum [ sum [ dist center p | let center = mean cl, p<-cl ] | cl<-clusters]
+
+-- Small, useful IO-related function
+printClusters :: Clusterizable a => [[a]] -> String
+printClusters clusters = concat $ zipWith (\ cl idx  -> "Cluster " ++ show idx ++ ":\n" ++ (printCl cl) ++ "\n") clusters [1..]
+  where printCl = unlines . map toFile
+
+-- impure Haskell = ugly Haskell
+-- ugly Haskell > ugly C++ :-P
+main = do
+    (inFilename, k, outFilename) <- parseArgs
+    putStr $ "Clustering into " ++ (show k) ++ " clusters... "
+    hFlush stdout
+    start <- getTime Realtime
+    contents <- readFile inFilename
+    let (points, n) = mapAndLength fromFile $ lines contents :: ([Point],Int)
+    if n < k then
+        putStrLn "[#] Error: #clusters should be < #datapoints!"
+    else do
+        let centerIdxs = take k . nub . randomRs (0, n-1) . mkStdGen . fromIntegral . toNanoSecs $ start
+            (clusters, iterations) = clusterize k centerIdxs points
+            minReached = wcss clusters
+        writeFile outFilename $ printClusters clusters
+        end <- getTime Realtime
+        putStrLn $ "done. (" ++ show iterations ++ " iterations, "
+                             ++ show (milliseconds start end) ++ "msec, wcss="
+                             ++ show minReached ++ ")"
+  where milliseconds start end = fromIntegral (toNanoSecs (diffTimeSpec start end)) / 10^6
+
+-- Improves 'main' readability
+parseArgs :: IO (String, Int, String)
+parseArgs = do
+    args <- getArgs
+    if length args < 3 then error "Inappropriate number of arguments!"
+    else let (a:b:c:_) = args in return (a, read b, c)
+
+-- User-friendly version - to be called from GHCI or WinGHCI
+kmeans :: String -> Int -> String -> IO ()
+kmeans inFilename k outFilename = withArgs [inFilename, (show k), outFilename] main
